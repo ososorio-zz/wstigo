b3c01bf7ec91656473d3c400d034febd19d2be56
oscarosorio88@gmail.com <oscarosorio88@gmail.com@ec273b2c-2314-3343-fcaf-986fd2cef755>
2011-10-12 03:52:36 +0000

    arreglos para validaciones

--- WSTigoAA/src/com/aa/logic/InformationMSISDN.java
+++ WSTigoAA/src/com/aa/logic/InformationMSISDN.java
@@ -51,7 +51,7 @@ public class InformationMSISDN implements Services {
 			response(writer,"{\"responseinfo\":{ \"error\":\""+message+"\",\"internError\":\""+e.getMessage()+"\"}}");
 		}
 		
-		
+	 	
 
 	}
 
